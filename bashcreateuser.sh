f30da2f79c9ac0a46dc4d6fb5cb507cb65bee6c7
unknown <amanda.wisniowski@gmail.com>
2025-02-12 21:36:16 -0500

    putting logic into functions rather than if else

--- bashcreateuser.sh
+++ bashcreateuser.sh
@@ -1,37 +1,56 @@
 #!/bin/bash
+
+get_password() {
+    while true; do
+        read -sp "Enter user password: " password
+        echo
+        read -sp "Confirm password: " password_confirm
+        echo
+        if [ "$password" == "$password_confirm" ]; then
+            echo "Passwords match."
+            break
+        else
+            echo "Passwords do not match. Please try again."
+        fi
+    done
+}
+
+get_permissions() {
+    perms="u=" 
+    declare -A perm_options=( ["read"]="r" ["write"]="w" ["execute"]="x" )
+
+    for perm in "${!perm_options[@]}"; do
+        read -p "Do you want to allow $perm access? (y/n) " response
+        if [[ $response =~ ^[Yy]$ ]]; then
+            perms+="${perm_options[$perm]}"
+        fi
+    done
+
+    echo "$perms"
+}
+
+
 read -p "Enter username: " name
-read -sp "Enter user password: " password
-echo
-read -sp "Confirm password: " password_confirm
+if [[ -z "$name" ]]; then
+    echo "Username cannot be empty."
+    exit 1
+fi
 
-result_perm="u=" 
 
-if [ "$password" == "$password_confirm" ]; then
-  echo
-  echo "Passwords match."
-  read -p "Do you want to allow read access? (y/n) " read_perm
-  if [[ $read_perm == "y" ]]; then
-    result_perm+="r"
-  fi
-  read -p "Do you want to allow write access? (y/n) " write_perm
-  if [[ $write_perm == "y" ]]; then
-    result_perm+="w"
-  fi
-  read -p "Do you want to allow execute access? (y/n) " exec_perm
-  if [[ $exec_perm == "y" ]]; then
-    result_perm+="x"
-  fi
-  echo "Permissions: $result_perm"
- 
-  sudo adduser "$name"
-  sleep 1
-  sudo chown -R "$name:$name" "/home/$name"
-  sudo chmod -R "$result_perm" "/home/$name"
-  echo "Permissions applied to /home/$name."
+get_password
+
+result_perm=$(get_permissions)
+echo "Selected permissions: $result_perm"
+
+
+if sudo adduser --gecos "" "$name"; then
+    echo "User '$name' created successfully."
+
+
+    sudo chown -R "$name:$name" "/home/$name"
+    sudo chmod -R "$result_perm" "/home/$name"
+    echo "Permissions applied to /home/$name."
 else
-  echo
-  echo "Passwords do not match. Please try again."
-  read -sp "Enter user password: " password
-  echo
-  read -sp "Confirm password: " password_confirm
+    echo "Error: Failed to create user."
+    exit 1
 fi
